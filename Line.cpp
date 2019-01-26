d66d2d219a6ac867aee38bf3b3b9c138872eb716
daphtya <davidpanjaitan1@gmail.com>
2019-01-26 12:34:37 +0700

    bikin animasi garis tapi belum dites karena belum bikin draw line

--- Line.cpp
+++ Line.cpp
@@ -15,17 +15,19 @@ class Line {
 	public:
 		int x1,y1,x2,y2;
 		int animationTime;
+		int timePassed;
 
 	Line(){
 		this->x1 = 0;
 		this->x2 = 0;
 		this->y1 = 0;
 		this->y2 = 0;
+		this->animationTime = 0;
+		this->timePassed = 0;
 	}
 
 	Line(int x1, int y1, int x2, int y2){
 		this->setLine(x1,y1,x2,y2);
-		this->animationTime = 0;
 	}
 
 	Line(int x1, int y1, int x2, int y2, int animationTime) : Line(x1,y1,x2,y2) {
@@ -39,13 +41,30 @@ class Line {
 		this->y2 = y2;
 	}
 
-	void draw(char* fbp){
+	void draw(char* fbp, char color[3]){
 
 	}
 
-	void animateDrawing(char* fbp, int dt){
+	void draw(char* fbp, int x2, int y2, char color[3]) {
 
+	} 
+
+	void animateDrawing(char* fbp, int dt, int color_num, char (*colors)[3]){
+		if (animationTime == 0)
+			return;
+		float phase = (float) timePassed / animationTime; //calculate phase
+		i = 0;
+		while (i < color_num) { // search for the appropriate color for this phase
+			float colorphase = ((float) i + 1)/color_num;
+			if (phase > colorphase)
+				break;
+			i++;
+		}
+
+		int tempx2 = x1 + (int)((x2-x1) * phase);
+		int tempy2 = y1 + (int)((y2-y1) * phase);
+		this.draw(fbp, tempx2, tempy2, colors[i]);
+		timePassed += dt;
 	}
-
 
 };
